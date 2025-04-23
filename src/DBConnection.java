bfee0accb30a11421bd8227d8efed8547cefd8c5
JDorczuk28 <jack.dorczuk@western.edu>
2025-04-23 09:31:11 -0600

    Add DBConnection

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,2 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private static final String URI = "jdbc:sqlite:sample.db";
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(URI);
+
+
+        return connection;
+
+    }
+
+    public static void closeConnection() throws SQLException{
+        connection.close();
+    }
+
 }
