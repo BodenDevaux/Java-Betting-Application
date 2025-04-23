efc53dd59f47667c61ece31804081bd270ded529
JDorczuk28 <jack.dorczuk@western.edu>
2025-04-23 09:40:46 -0600

    Tweaked names

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URI = "jdbc:sqlite:sample.db";
+    private static final String URI = "jdbc:sqlite:users.db";
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
