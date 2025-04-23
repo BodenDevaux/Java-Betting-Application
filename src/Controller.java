821ee05f7b7247b65fd4dcc9cfd635eeb3f2d46c
JDorczuk28 <jack.dorczuk@western.edu>
2025-04-23 09:53:17 -0600

    Started Database Stuff

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,33 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Controller {
+    private Connection connection;
+    public Controller(){
+        try {
+            this.connection = DBConnection.getConnection();
+            System.out.println("Connection Successful");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void addTable(){
+        String cmd = "CREATE TABLE IF NOT EXISTS users(" +
+                "user_id INTEGER PRIMARY KEY," +
+                "username TEXT NOT NULL" +
+                "password TEXT NOT NULL" +
+                ");";
+
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(cmd);
+            System.out.println("Table Create or Alreaady exists");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void addUser(String username, String password){
+
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Server {
+    static Controller controller = new Controller();
     public static void main(String[] args) throws IOException {
+        System.out.println("Enter Username: ");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String username = bufferedReader.readLine();
+        System.out.println("Enter Password: ");
+        String password = bufferedReader.readLine();
+        controller.addTable();
+
 
     }
 }
