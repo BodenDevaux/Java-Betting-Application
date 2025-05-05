e295f777abcdcabbbab2c53d73cc46240f0b3f95
JDorczuk28 <jack.dorczuk@western.edu>
2025-05-04 22:25:47 -0600

    password hashing

--- src/authenticateModel.java
+++ src/authenticateModel.java
@@ -1,3 +1,5 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 public class authenticateModel {
@@ -34,7 +36,8 @@ public class authenticateModel {
     public void addUser(String userInfo){
         String[] info = userInfo.split(",");
         String username = info[0];
-        String password = info[1];
+        //String password = info[1];
+        String password = hashPassword(info[1]);
 
         String cmd = "INSERT into users (username,password) VALUES(?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(cmd)) {
@@ -49,7 +52,8 @@ public class authenticateModel {
     public boolean Login(String info) {
         String[] userinfo = info.split(",");
         String username = userinfo[0];
-        String password = userinfo[1];
+        //String password = userinfo[1];
+        String password = hashPassword(userinfo[1]);
         String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
@@ -74,5 +78,23 @@ public class authenticateModel {
     }
     public Player getPlayer(){
         return player;
+    }
+    //add password hashing
+    //https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+    private static String hashPassword(String passwordToHash){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(passwordToHash.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 }
